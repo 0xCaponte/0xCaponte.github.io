238fc408bc450eaf6ade5c9ce491db54b2ce40e6
Carlos <2414186+0xCaponte@users.noreply.github.com>
2023-11-28 23:10:46 +0100

    PurgeCSS: font-awesome and materialize
    PurgeCSS: font-awesoome and materialize

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    content: ["./_site/**/*.html"],
-    css: ["./_site/css/*.css"],
-  
-    // Tailwind CSS config
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-  };
+  content: ['./_site/**/*.html'],
+  css: ['./_site/css/font-awesome.min.css', './_site/css/materialize.min.css'],
+
+  // Tailwind CSS config
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
+}
